bc1d1ca1571e5b7c68af3b505474b3932b46bad8
KAVEENESH <Kaveenesh2014@gmail.com>
2025-12-25 13:46:23 +0530

    initial commit

--- i.py
+++ i.py
@@ -1,3 +1,5 @@
-a = input('pls input binary num:')
-b = int(a, 2)
-print("Decimal value is:", b)
+a = input('input a binary number: ')
+while True:
+    b = int(a, 2)
+    print("Decimal value is:", b)
+    break
